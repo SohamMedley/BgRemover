ed5534716ee745586ea62f1135e5acba4a16b0b7
Soham <dealwithsoham@gmail.com>
2025-03-10 16:00:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Install pyenv
-curl https://pyenv.run | bash
-
-# Add pyenv to PATH
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-
-# Install Python
-pyenv install 3.8.12
-pyenv global 3.8.12
-
-# Install dependencies
+# Upgrade pip and install dependencies
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-#temp
+echo "Build complete."
